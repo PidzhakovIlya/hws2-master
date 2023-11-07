1182fcda4daa96bf43ca66f48b339ca5836d00f4
PidzhakovIlya <pidzhakovilja@gmail.com>
2023-11-07 21:55:00 +0300

    hw 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,35 @@
-import {UserType} from '../HW8'
+import {UserType} from "../HW8"
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+    | { type: "sort"; payload: "up" | "down" }
+    | { type: "check"; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case "sort": { // by name
+            let stateCopy = [...state]
+            if (action.payload === "up") {
+                stateCopy.sort((a, b) => {
+                    if (a.name < b.name) {
+                        return -1;
+                    }else {
+                        return 0
+                    }
+                })
+            }else if (action.payload === "down") {
+                stateCopy.sort((a, b) => {
+                    if (a.name > b.name) {
+                        return 1;
+                    }else {
+                        return 0
+                    }
+                })
+            }// need to fix
+            return stateCopy
         }
-        case 'check': {
-
-            return state // need to fix
+        case
+        "check": {
+            return [...state.filter(el=>el.age>18)]// need to fix
         }
         default:
             return state
