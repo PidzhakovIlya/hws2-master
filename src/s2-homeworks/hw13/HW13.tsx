20cff32d55b9811a2c1f81a9e014babdd1eee8e7
PidzhakovIlya <pidzhakovilja@gmail.com>
2024-04-29 18:24:55 +0300

    hw-13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -1,12 +1,12 @@
-import React, {useState} from 'react'
-import s2 from '../../s1-main/App.module.css'
-import s from './HW13.module.css'
-import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
-import axios from 'axios'
-import success200 from './images/200.svg'
-import error400 from './images/400.svg'
-import error500 from './images/500.svg'
-import errorUnknown from './images/error.svg'
+import React, {useState} from "react"
+import s2 from "../../s1-main/App.module.css"
+import s from "./HW13.module.css"
+import SuperButton from "../hw04/common/c2-SuperButton/SuperButton"
+import axios, {AxiosError, AxiosResponse} from "axios"
+import success200 from "./images/200.svg"
+import error400 from "./images/400.svg"
+import error500 from "./images/500.svg"
+import errorUnknown from "./images/error.svg"
 
 /*
 * 1 - дописать функцию send
@@ -15,73 +15,100 @@ import errorUnknown from './images/error.svg'
 * */
 
 const HW13 = () => {
-    const [code, setCode] = useState('')
-    const [text, setText] = useState('')
-    const [info, setInfo] = useState('')
-    const [image, setImage] = useState('')
+    const [code, setCode] = useState("")
+    const [text, setText] = useState("")
+    const [info, setInfo] = useState("")
+    const [image, setImage] = useState("")
+    const [isDisabled, setIsDisabled] = useState(false)
 
     const send = (x?: boolean | null) => () => {
         const url =
             x === null
-                ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
-                : 'https://samurai.it-incubator.io/api/3.0/homework/test'
+                ? "https://xxxxxx.ccc" // имитация запроса на не корректный адрес
+                : "https://samurai.it-incubator.io/api/3.0/homework/test"
 
-        setCode('')
-        setImage('')
-        setText('')
-        setInfo('...loading')
+        setCode("")
+        setImage("")
+        setText("")
+        setInfo("...loading")
+        setIsDisabled(true)
 
         axios
-            .post(url, {success: x})
+            .post<AxiosResponse, AxiosResponse<ResponseType>, {
+                success: boolean | null | undefined
+            }>(url, {success: x})
             .then((res) => {
-                setCode('Код 200!')
+                setCode("Код 200!")
                 setImage(success200)
-                // дописать
+                setText(res.data.errorText)
+                setInfo(res.data.info)
 
             })
             .catch((e) => {
-                // дописать
+                if (e.code === "ERR_BAD_REQUEST") {
+                    console.log(e.response.data.errorText)
+                    setCode("Ошибка 400!")
+                    setImage( error400)
+                    setText(e.response.data.errorText)
+                    setInfo(e.response.data.info)
+                }else if(e.code === "ERR_BAD_RESPONSE") {
+                    setCode("Ошибка 500!")
+                    setImage(error500)
+                    setText(e.response.data.errorText)
+                    setInfo(e.response.data.info)
+                }else if(e.code === "ERR_NETWORK"){
+                    setCode("Error!")
+                    setImage(errorUnknown)
+                    setText(e.message)
+                    setInfo(e.name)
+                }
 
-            })
+            }).finally(()=> {
+                setIsDisabled(false)
+            }
+        )
     }
 
     return (
-        <div id={'hw13'}>
+        <div id={"hw13"}>
             <div className={s2.hwTitle}>Homework #13</div>
 
             <div className={s2.hw}>
                 <div className={s.buttonsContainer}>
                     <SuperButton
-                        id={'hw13-send-true'}
+                        id={"hw13-send-true"}
                         onClick={send(true)}
-                        xType={'secondary'}
-                        // дописать
+                        xType={"secondary"}
+                        disabled={isDisabled}
 
                     >
                         Send true
                     </SuperButton>
                     <SuperButton
-                        id={'hw13-send-false'}
+                        id={"hw13-send-false"}
                         onClick={send(false)}
-                        xType={'secondary'}
+                        xType={"secondary"}
                         // дописать
+                        disabled={isDisabled}
 
                     >
                         Send false
                     </SuperButton>
                     <SuperButton
-                        id={'hw13-send-undefined'}
+                        id={"hw13-send-undefined"}
                         onClick={send(undefined)}
-                        xType={'secondary'}
+                        xType={"secondary"}
+                        disabled={isDisabled}
                         // дописать
 
                     >
                         Send undefined
                     </SuperButton>
                     <SuperButton
-                        id={'hw13-send-null'}
+                        id={"hw13-send-null"}
                         onClick={send(null)} // имитация запроса на не корректный адрес
-                        xType={'secondary'}
+                        xType={"secondary"}
+                        disabled={isDisabled}
                         // дописать
 
                     >
@@ -95,13 +122,13 @@ const HW13 = () => {
                     </div>
 
                     <div className={s.textContainer}>
-                        <div id={'hw13-code'} className={s.code}>
+                        <div id={"hw13-code"} className={s.code}>
                             {code}
                         </div>
-                        <div id={'hw13-text'} className={s.text}>
+                        <div id={"hw13-text"} className={s.text}>
                             {text}
                         </div>
-                        <div id={'hw13-info'} className={s.info}>
+                        <div id={"hw13-info"} className={s.info}>
                             {info}
                         </div>
                     </div>
@@ -112,3 +139,12 @@ const HW13 = () => {
 }
 
 export default HW13
+
+
+type ResponseType = {
+    errorText: string
+    info: string
+    yourBody: { success: boolean }
+    yourQuery: {}
+
+}
